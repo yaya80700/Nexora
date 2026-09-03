cb80ea7f113837437e2e8b76523d788c3ad22ad8
yaya80700 <88847270+yaya80700@users.noreply.github.com>
2026-09-03 19:13:55 +0200

    Delete app directory

--- app/ui/Logo.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import Link from "next/link";
-
-export default function Logo({compact=false}) {
-  return <Link href="/" className={compact ? "logo compact" : "logo"} aria-label="Nexora - Accueil">
-    <Image src="/nexora-logo.png" alt="Nexora" width={42} height={42} priority />
-    {!compact && <span>NEXORA</span>}
-  </Link>;
-}
