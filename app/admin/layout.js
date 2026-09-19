7701fdf70ab7eb05a92e225c433dac6591432147
yaya80700 <88847270+yaya80700@users.noreply.github.com>
2026-09-20 01:31:45 +0200

    Delete app directory

--- app/admin/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Administration",
-  robots: { index: false, follow: false },
-};
-
-export default function PrivateLayout({ children }) {
-  return children;
-}
--- app/compte/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Mon espace",
-  robots: { index: false, follow: false },
-};
-
-export default function PrivateLayout({ children }) {
-  return children;
-}
