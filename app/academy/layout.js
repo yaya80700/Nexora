eaa972a1377b13a03676cd2b5d0a310853a9ff5e
yaya80700 <88847270+yaya80700@users.noreply.github.com>
2026-09-23 23:08:55 +0200

    Delete app directory

--- app/academy/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Nexora Academy",
-  robots: { index: false, follow: false },
-};
-
-export default function PrivateLayout({ children }) {
-  return children;
-}
--- app/demandes/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Mes demandes",
-  robots: { index: false, follow: false },
-};
-
-export default function PrivateLayout({ children }) {
-  return children;
-}
