08bac59b276eb4e41cd8f70029b59fbcd8a947c7
yaya80700 <88847270+yaya80700@users.noreply.github.com>
2026-09-24 18:28:34 +0200

    Delete app directory

--- app/reinitialiser-mot-de-passe/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Réinitialiser le mot de passe",
-  robots: { index: false, follow: false },
-};
-
-export default function PrivateLayout({ children }) {
-  return children;
-}
