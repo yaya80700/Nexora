c54e61e6bf7c4a5f621b2ce074f0b33d15195dde
yaya80700 <88847270+yaya80700@users.noreply.github.com>
2026-09-24 18:12:05 +0200

    Delete app directory

--- app/mot-de-passe-oublie/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Mot de passe oublié",
-  robots: { index: false, follow: false },
-};
-
-export default function PrivateLayout({ children }) {
-  return children;
-}
